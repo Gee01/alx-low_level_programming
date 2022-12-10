ce9ca2d73b797f5df25da5752abef4e6d1f26175
Gee01 <gaiusfort@gmail.com>
2022-12-10 10:29:25 -0800

    Advanced task 1

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,15 +13,15 @@ int main(void)
 	int ones = '0';
 	int tens = '0';
 
-	for (tens = '0'; tens <= '0'; tens++)/* prints tens digit*/
+	for (tens = '0'; tens <= '9'; tens++)/* prints tens digit*/
 	{
-		for (ones = '0'; ones <= '0'; ones++)/* prints ones digits*/
+		for (ones = '0'; ones <= '9'; ones++)/* prints ones digits*/
 		{
-			if (!((ones == tens) || {tens > ones))}/*eliminates repetition*/
+			if (!((ones == tens) || (tens > ones)))/*eliminates repetition*/
 			{
 				putchar(tens);
 				putchar(ones);
-				if (!(ones == '0' && tens == '0'))/*adds comma and space*/
+				if (!(ones == '9' && tens == 'g'))/*adds comma and space*/
 				{
 					putchar(',');
 					putchar(' ');
@@ -29,7 +29,7 @@ int main(void)
 			}
 		}
 	}
-	
+
 	putchar('\n');
 
 	return (0);
