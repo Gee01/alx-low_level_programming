78c64867a28fc0658a47044b6b80f48c602c7cbb
Gee01 <gaiusfort@gmail.com>
2023-01-02 18:42:26 -0800

    Advanced Task 1

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,21 @@
+/*
+ * File: 100-set_string.c
+ * Auth Gee01 Advanced Task 1
+ */
+
+#include "main.h"
+
+/**
+ * set_string - sets the value of a pointer to a char
+ *
+ * @s: source adress.
+ *
+ * @to: target adress.
+ *
+ * Return: no return.
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
