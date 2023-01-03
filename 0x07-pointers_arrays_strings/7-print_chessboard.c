6096dff80cba4f731b6e2a2ddc95eff3182ac1fd
Gee01 <gaiusfort@gmail.com>
2023-01-02 18:29:52 -0800

    Task 7

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,27 @@
+/*
+ * File: 7-print_chessboard.c
+ * Auth: Gee01 Task 7
+ */
+
+#include "main.h"
+
+/**
+ * print_chessboard - prints chessboard
+ *
+ * @a: matrix
+ */
+
+void print_chessboard(char (*a)[8])
+
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(*(*(i + a) + j));
+		}
+		_putchar('\n');
+	}
+}
