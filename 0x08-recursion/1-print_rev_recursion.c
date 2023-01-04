2d6876d596cf7489fce46aa6175a51a019d18c5d
Gee01 <gaiusfort@gmail.com>
2023-01-04 11:13:43 -0800

    Task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Prints a string,followed by a new line
+ *
+ * @s: This is the string
+ *
+ * Return: Void
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s > '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
