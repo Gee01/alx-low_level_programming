3297d65ecad11cf6c0b94251e37412d841e25b61
Gee01 <gaiusfort@gmail.com>
2023-01-04 12:52:07 -0800

    Task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,16 +2,14 @@
 
 /**
  * _sqrt_recursion - find natural square root
- *
  * @n: int
- *
  * Return: int
  */
-
 int _sqrt_recursion(int n)
 {
 	return (square(n, 1));
 }
+
 /**
  * square - find square root
  * @n: int to find square root
