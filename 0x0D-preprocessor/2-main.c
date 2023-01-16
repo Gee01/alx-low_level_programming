0b213c07bdb734a27c5265e21d23551e00d63b5c
Gee01 <gaiusfort@gmail.com>
2023-01-16 08:51:32 -0800

    Task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - prints name of any file that runs this code
+ *
+ * Return: 0
+ * */
+int main(void)
+{
+	printf("%s\n", __BASE_FILE__);
+	return (0);
+}
